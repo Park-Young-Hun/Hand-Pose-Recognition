8090f8c94c3a800c1adeae772d6191dc8d038b4b
Park-Young-Hun <55652627+Park-Young-Hun@users.noreply.github.com>
2020-08-29 20:31:29 +0900

    Update camera.py

--- Camera_module/camera.py
+++ Camera_module/camera.py
@@ -1,5 +1,4 @@
 import cv2
-import time
 
 global pnum
 pnum=0
@@ -22,27 +21,27 @@ if cam.isOpened() == False: #카메라 생성 확인
 
 print('width :%d, height : %d' % (cam.get(3), cam.get(4)))
 text = 'Smile~'
-count = 100
+#count = 100
 
 while(True):
     ret, frame = cam.read()    # Read 결과와 frame
 
     if(ret) :
 
-
-
+        cv2.imshow('web_cam', frame)
 
         if model():
 
+          for i in range(0, 1000) :
+
+            ret, frame1 = cam.read()
+            cv2.putText(frame1, text, (100, 100), cv2.FONT_ITALIC, 1, (255, 0, 0))
+            cv2.imshow('web_cam', frame1)
 
 
-            cv2.putText(frame, text, (100, 100), cv2.FONT_ITALIC, 1, (255, 0, 0))
-            #cv2.imshow('web_cam', frame)
+            #cv2.imwrite('picture' + str(pnum) + '.jpg', frame)
 
 
-            cv2.imwrite('picture' + str(pnum) + '.jpg', frame)
-
-        cv2.imshow('web_cam', frame)
 
         if cv2.waitKey(1) == 27:
             break
