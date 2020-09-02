44d8d7e451a75f2c81e60fca0f6b1aa3eb9711a5
Park-Young-Hun <55652627+Park-Young-Hun@users.noreply.github.com>
2020-09-02 18:47:59 +0900

    camera_module(20200902)

--- Camera_module/camera.py
+++ Camera_module/camera.py
@@ -1,7 +1,8 @@
 import cv2
+from tkinter import messagebox
 
 global pnum
-pnum=0
+pnum = 0
 
 def model() :
     while(True):
@@ -13,42 +14,39 @@ def model() :
         else :
             return False
 
-cam = cv2.VideoCapture(0)
-
-if cam.isOpened() == False: #카메라 생성 확인
-    print ('CAM Error')
-    exit()
-
-print('width :%d, height : %d' % (cam.get(3), cam.get(4)))
-text = 'Smile~'
-#count = 100
-
-while(True):
-    ret, frame = cam.read()    # Read 결과와 frame
-
-    if(ret) :
-
-        cv2.imshow('web_cam', frame)
-
-        if model():
-
-          for i in range(0, 1000) :
-
-            ret, frame1 = cam.read()
-            cv2.putText(frame1, text, (100, 100), cv2.FONT_ITALIC, 1, (255, 0, 0))
-            cv2.imshow('web_cam', frame1)
+def camera() :
 
 
-            #cv2.imwrite('picture' + str(pnum) + '.jpg', frame)
+    cam = cv2.VideoCapture(0)
 
+    if cam.isOpened() == False:  # 카메라 생성 확인
+        print('CAM Error')
+        exit()
 
+    print('width :%d, height : %d' % (cam.get(3), cam.get(4)))
+    text = 'Smile~'
+    # count = 100
+
+    while (True):
+        ret, frame = cam.read()  # Read 결과와 frame
+
+        if (ret):
+
+            cv2.imshow('web_cam', frame)
+
+            if model():
+
+                messagebox.showinfo("Taking a picture", "Smile~")
+                #cv2.putText(frame, text, (100, 100), cv2.FONT_ITALIC, 1, (255, 0, 0))
+                cv2.imwrite('picture' + str(pnum) + '.jpg', frame)
 
         if cv2.waitKey(1) == 27:
             break
-cam.release()
-cv2.destroyAllWindows()
+    cam.release()
+    cv2.destroyAllWindows()
 
 
+camera()
 
 
 
